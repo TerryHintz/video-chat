e2a085f1335b7d9c4e7c5a74aed87266551d6673
TerryHintz <cza82@sfu.ca>
2020-07-09 11:57:16 -0700

    structure set up

---
+++ src/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Dashboard extends Component {
+    render(){
+        return(
+            <div>
+                temp
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
